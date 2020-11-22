cb1968535563650a9befbf5cbda9bf323f605df5
Jordan Loeser <loeserjordan@gmail.com>
2020-11-22 02:15:08 -0500

    Successfully pull items from group

--- src/App.js
+++ src/App.js
@@ -9,17 +9,69 @@ class App extends React.Component {
 
     // Default state
     this.state = {
+      loading: false,
       settings: {},
-      name: "",
+      context: {},
+      items: [],
     };
   }
 
+  pullGroupData() {
+    if (!this.state.settings.backlog_group) return;
+    this.setState({ loading: true });
+    monday
+      .api(
+        `query ($boardIds: [Int], $groupIds: [String]) {
+          boards(ids:$boardIds, limit:1) {
+            groups(ids:$groupIds) {
+              items {
+                id
+                name
+              }
+            }
+          }
+        }`,
+        {
+          variables: {
+            boardIds: this.state.context.boardIds,
+            groupIds: this.state.settings.backlog_group,
+          },
+        }
+      )
+      .then((res) => {
+        const { groups } = res.data.boards[0];
+        if (groups.length > 0) {
+          this.setState({ items: groups[0].items });
+        }
+        this.setState({ loading: false });
+      });
+  }
+
   componentDidMount() {
-    // TODO: set up event listeners
+    monday.listen("settings", (res) => {
+      this.setState({ settings: res.data });
+      this.pullGroupData();
+    });
+    monday.listen("context", (res) => {
+      this.setState({ context: res.data });
+      // TODO: confirm there's no situation where this could change on the same instance
+      // this.pullGroupData();
+    });
   }
 
   render() {
-    return <div className="App">Hello, monday Apps!</div>;
+    return (
+      <div className="App">
+        <div>
+          <p>{this.state.settings?.backlog_group}</p>
+          <p>
+            {this.state.loading
+              ? "LOADING..."
+              : JSON.stringify(this.state.items, null, 1)}
+          </p>
+        </div>
+      </div>
+    );
   }
 }
 
