de73845552e2cdc29a526ce779535a0e1a5cce0f
Jordan Loeser <loeserjordan@gmail.com>
2020-11-23 22:13:11 -0500

    add tooltip

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,22 @@
 import React, { useContext } from "react";
 import styled from "styled-components";
-// import Moment from "moment";
-import { Button } from "monday-ui-react-core";
+import Moment from "moment";
+import { Button, Tooltip } from "monday-ui-react-core";
+import Icon from "monday-ui-react-core/dist/Icon";
+import Activity from "monday-ui-react-core/dist/icons/Activity";
 import { MondayContext } from "../App";
 
-// const formatDate = (dateString) => {
-//   const date = new Moment(dateString);
-//   return date.calendar({
-//     sameDay: "[Today]",
-//     nextDay: "[Tomorrow]",
-//     nextWeek: "dddd",
-//     lastDay: "[Yesterday]",
-//     lastWeek: "[Last] dddd",
-//     sameElse: "DD/MM/YYYY",
-//   });
-// };
+const formatDate = (dateString) => {
+  const date = new Moment(dateString);
+  return date.calendar({
+    sameDay: "[Today]",
+    nextDay: "[Tomorrow]",
+    nextWeek: "dddd",
+    lastDay: "[Yesterday]",
+    lastWeek: "[Last] dddd",
+    sameElse: "DD/MM/YYYY",
+  });
+};
 
 const CardContainer = styled.div`
   display: flex;
@@ -47,13 +49,12 @@ const Title = styled.h1`
   flex: 1;
 `;
 
-// const MetaContainer = styled.div`
-//   display: flex;
-//   flex-direction: row;
-//   padding: 16px 24px;
-//   font-size: 14px;
-//   line-height: 1.4;
-// `;
+const MetaContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 // const Meta = styled.div`
 //   flex: 1;
@@ -69,40 +70,73 @@ const Title = styled.h1`
 //   margin: 0;
 // `;
 
+const TooltipContainer = styled.div`
+  //   position: relative;
+  //   bottom: -10px;
+`;
+
 const StyledButton = styled(Button)`
-  align-self: flex-end;
+  //   align-self: flex-end;
   //   margin-bottom: 8px;
 `;
 
 const Card = ({ item }) => {
   const monday = useContext(MondayContext);
+  console.log("***", Icon);
   return (
     <CardContainer>
       <TitleContainer>
         <Title>{item.name}</Title>
-        <StyledButton
-          size={Button.sizes.SMALL}
-          kind={Button.kinds.SECONDARY}
-          color={Button.colors.ON_PRIMARY_COLOR}
-          onClick={() => {
-            // TODO: updatee to `kind: "columns"` after API is fixed
-            monday.execute("openItemCard", {
-              itemId: item.id,
-              kind: "updates",
-            });
-          }}
-        >
-          View Item
-        </StyledButton>
+        <MetaContainer>
+          <TooltipContainer>
+            <Tooltip
+              showDelay={300}
+              position={"top"}
+              moveBy={{ main: 16, secondary: 0 }}
+              content={
+                <>
+                  <div>
+                    <b>Created:</b> {formatDate(item.created_at)}
+                  </div>
+                  <div>
+                    <b>Updated:</b> {formatDate(item.updated_at)}
+                  </div>
+                </>
+              }
+            >
+              <Icon
+                iconType={"SVG"}
+                icon={Activity}
+                iconLabel="my bolt svg icon"
+                clickable
+                iconSize={24}
+              />
+            </Tooltip>
+          </TooltipContainer>
+          <StyledButton
+            size={Button.sizes.SMALL}
+            kind={Button.kinds.SECONDARY}
+            color={Button.colors.ON_PRIMARY_COLOR}
+            onClick={() => {
+              // TODO: updatee to `kind: "columns"` after API is fixed
+              monday.execute("openItemCard", {
+                itemId: item.id,
+                kind: "updates",
+              });
+            }}
+          >
+            View Item
+          </StyledButton>
+        </MetaContainer>
       </TitleContainer>
       {/* <MetaContainer>
             <Meta>
             <MetaTitle>Created</MetaTitle>
-            <MetaData>{formatDate(item.created_at)}</MetaData>
+            <MetaData>{}</MetaData>
             </Meta>
             <Meta>
             <MetaTitle>Updated</MetaTitle>
-            <MetaData>{formatDate(item.updated_at)}</MetaData>
+            <MetaData>{}</MetaData>
             </Meta>
             </MetaContainer> */}
     </CardContainer>
